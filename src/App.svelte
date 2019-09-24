e864e137ea917c0ecb46614a393783dffc082c8c
Thomas Collardeau <thomas@collardeau.com>
2019-09-24 19:01:56 +0100

    use router and header

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,14 @@
+<script>
+  import Header from "./components/Header.svelte";
+  import { route } from "./stores/route";
+
+  let title = "My New App";
+  let links = [{ id: "Home", href: "#" }, { id: "About", href: "#about" }];
+</script>
+
 <div class="antialiased text-gray-900">
-  <header>
-    <h1 class="font-semibold text-xl">Header</h1>
-  </header>
-  <main>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi molestiae
-    dolor quibusdam corrupti ex quidem! Autem iusto obcaecati aperiam tenetur,
-    necessitatibus laboriosam aliquam a adipisci reprehenderit consectetur
-    repudiandae eos ab numquam quisquam reiciendis sequi et eum amet dignissimos
-    sit animi eius earum eaque. Velit, tenetur amet dolorum itaque reiciendis a
-    delectus quod praesentium, totam quia perspiciatis blanditiis! Voluptate, in
-    culpa. Magni pariatur omnis consectetur voluptate doloremque, a eum tenetur
-    illo unde perferendis, culpa soluta, molestiae placeat quibusdam error
-    architecto aperiam officia. Perferendis, quia expedita enim id est doloribus
-    consequuntur nesciunt, saepe dolore dolorum, dicta cupiditate ea odio
-    reprehenderit aut nostrum!
+  <Header {title} {links} />
+  <main class="px-2">
+    {#if $route === 'about'}About{:else}Home{/if}
   </main>
 </div>
