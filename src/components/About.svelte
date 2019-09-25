94a6c32134e09a0e5461b9dac7e699a34618e5d0
Thomas Collardeau <thomas@collardeau.com>
2019-09-25 23:31:51 +0100

    add animation on about page

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,14 +1,21 @@
-<h3 class="text-2xl mt-4 font-semibold">About</h3>
+<script>
+  import { fade } from "svelte/transition";
+</script>
 
-<p class="">
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima rerum at error
-  eos quam quo sapiente porro similique iste, repellendus enim reiciendis!
-  Aliquam eveniet magni reiciendis quod, ea, voluptates quia quasi corporis vero
-  ipsam quam, tempore rem voluptas provident nobis porro mollitia deleniti! Vero
-  aut porro neque, error accusantium quibusdam. Recusandae, doloribus inventore?
-  Sapiente quaerat corporis eveniet possimus nobis suscipit maiores explicabo
-  pariatur mollitia? Quos sapiente voluptates vel maxime autem a repellat
-  reprehenderit magnam quae commodi vitae et, velit eos. Earum minus delectus
-  quia deleniti ipsam autem adipisci, officia iure! Porro corporis quos debitis,
-  fugit non consectetur facere vero enim.
-</p>
+<section in:fade={{ duration: 500 }}>
+
+  <h3 class="text-2xl mt-4 font-semibold">About</h3>
+
+  <p class="">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima rerum at
+    error eos quam quo sapiente porro similique iste, repellendus enim
+    reiciendis! Aliquam eveniet magni reiciendis quod, ea, voluptates quia quasi
+    corporis vero ipsam quam, tempore rem voluptas provident nobis porro
+    mollitia deleniti! Vero aut porro neque, error accusantium quibusdam.
+    Recusandae, doloribus inventore? Sapiente quaerat corporis eveniet possimus
+    nobis suscipit maiores explicabo pariatur mollitia? Quos sapiente voluptates
+    vel maxime autem a repellat reprehenderit magnam quae commodi vitae et,
+    velit eos. Earum minus delectus quia deleniti ipsam autem adipisci, officia
+    iure! Porro corporis quos debitis, fugit non consectetur facere vero enim.
+  </p>
+</section>
