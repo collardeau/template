9e8d14c5666cf7004497098eec0d3882e48a37c0
Thomas Collardeau <thomas@collardeau.com>
2019-09-25 16:49:51 +0100

    add max width for feed

--- src/components/Feed.svelte
+++ src/components/Feed.svelte
@@ -6,15 +6,17 @@
   {#if $postsLoading}
     <p>Loading...</p>
   {:else}
-    {#each $posts as { img, title, createdOn }}
-      <article class="bg-white mt-4">
-        <img class="h-32 w-full object-cover" src={img} alt="test-image" />
-        <section class="p-2 ">
-          <h3 class="text-2xl font-semibold capitalize p-0 m-0">{title}</h3>
-          <p class="text-gray-500">posted on {createdOn}</p>
-        </section>
-      </article>
-    {/each}
+    <section class="max-w-xl mx-auto">
+      {#each $posts as { img, title, createdOn }}
+        <article class="bg-white mt-4">
+          <img class="h-32 w-full object-cover" src={img} alt="test-image" />
+          <section class="p-2 ">
+            <h3 class="text-2xl font-semibold capitalize p-0 m-0">{title}</h3>
+            <p class="text-gray-500">posted on {createdOn}</p>
+          </section>
+        </article>
+      {/each}
+    </section>
   {/if}
 
 </section>
