f455a70aabbf7d5fdd97948fcbbd601740630141
Thomas Collardeau <thomas@collardeau.com>
2019-09-25 12:24:32 +0100

    use mock data and store

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -1,13 +1,20 @@
 <script>
-
+  import { posts, postsLoading } from "../stores/posts";
 </script>
 
-<section class="pt-2">
-  <article class="bg-white">
-    <img
-      class="h-32 w-full object-cover"
-      src="http://lorempixel.com/400/200/abstract"
-      alt="test-image" />
-    <h3 class="text-3xl font-semibold">A Great Thing</h3>
-  </article>
+<section>
+  {#if $postsLoading}
+    <p>Loading...</p>
+  {:else}
+    {#each $posts as { img, title, createdOn }}
+      <article class="bg-white mt-4">
+        <img class="h-32 w-full object-cover" src={img} alt="test-image" />
+        <section class="p-2 ">
+          <h3 class="text-2xl font-semibold capitalize p-0 m-0">{title}</h3>
+          <p class="text-gray-500">posted on {createdOn}</p>
+        </section>
+      </article>
+    {/each}
+  {/if}
+
 </section>
