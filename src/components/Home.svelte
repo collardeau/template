a913170dac7ddb87333e3f00193207c3315a63a1
Thomas Collardeau <thomas@collardeau.com>
2019-09-25 10:46:32 +0100

    create a home comp

---
+++ src/components/Home.svelte
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<section class="pt-2">
+  <article class="bg-white">
+    <img
+      class="h-32 w-full object-cover"
+      src="http://lorempixel.com/400/200/abstract"
+      alt="test-image" />
+    <h3 class="text-3xl font-semibold">A Great Thing</h3>
+  </article>
+</section>
