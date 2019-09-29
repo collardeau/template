b53467cab2eff429a3f807c22c39edfde7e79fda
Thomas Collardeau <thomas@collardeau.com>
2019-09-29 16:02:39 +0100

    modify grid css

--- src/components/Grid.svelte
+++ src/components/Grid.svelte
@@ -2,12 +2,12 @@
   export let items = [];
 </script>
 
-<section class="flex flex-wrap justify-around">
+<section class="flex flex-wrap flex-between max-w-6xl mx-auto">
   {#each items as { id, img, title, createdOn }}
-    <article class="bg-white shadow mb-4 w-48 h-64">
+    <article class="px-2 mb-4 w-full sm:w-1/2 lg:w-1/3 xl:w-1/4">
       <a href="#/item/{id}">
-        <img class="h-32 cover" src={img} alt="item" />
-        <section class="p-2 ">
+        <img class="h-48 cover" src={img} alt="item" />
+        <section class="p-2 bg-white">
           <h3>{title}</h3>
           <p class="text-gray-500">posted on {createdOn}</p>
         </section>
