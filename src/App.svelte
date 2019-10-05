3214e4a416b3915433aca7bf90a6480ca44a4c65
Thomas Collardeau <thomas@collardeau.com>
2019-10-05 11:06:25 +0100

    create fluid header

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,14 @@
 <script>
-  import Router from "svelte-spa-router";
+  import Router, { location } from "svelte-spa-router";
   import { fade } from "svelte/transition";
   import Home from "./components/Home.svelte";
   import Page1 from "./components/pages/Page1.svelte";
   import Page2 from "./components/pages/Page2.svelte";
   import Item from "./components/Item.svelte";
   import About from "./components/About.svelte";
-  import Header from "./components/Header.svelte";
+  import Header from "./components/Fluid-Header.svelte";
 
-  let title = "demo gallery";
+  let title = "New Header";
 
   let routes = {
     "/": Home,
@@ -24,10 +24,35 @@
     { name: "animals", href: "#/animal" },
     { name: "about", href: "#/about" }
   ];
+
+  let toggleMenu;
+  const handleClick = () => {
+    toggleMenu();
+  };
 </script>
 
+<style>
+  div :global(.crazy) {
+    background-color: goldenrod;
+    padding: 10px;
+  }
+</style>
+
 <div class="app-wrapper">
-  <Header {title} {navLinks} />
+  <Header
+    bind:toggleMenu
+    bp="lg"
+    wrapperClass="bg-red-500 p-8"
+    buttonClass="foobar">
+    <div slot="left">
+      <h1>{title}</h1>
+    </div>
+    <!-- <div slot="right-small">
+      <button on:click={handleClick}>Toggle</button>
+    </div> -->
+    <div slot="horizontal">Horizontal links</div>
+    <div slot="menu-slider">Vertical links</div>
+  </Header>
   <main class="p-2">
     <Router {routes} />
   </main>
